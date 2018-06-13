9bc69ed41dd689b2d02ad4d0cf3c72e5df4c45fa
parkermuir <parker.muir@gmail.com>
2018-06-13 19:05:01 -0400

    Completed queue implentation for func-shared and prototypal

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  
+  instance.storage = {};
+  instance.first = 0;
+  instance.last = 0;
+
+  _.extend(instance, queueMethods);
+
+  return instance;
+
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(string) {
+  this.storage[this.last++] = string;
+};
+
+queueMethods.dequeue = function() {
+  if (this.first < this.last) {
+    var frontItem = this.storage[this.first];
+    delete this.storage[this.first++];
+    return frontItem;
+  }
+
+};
+
+queueMethods.size = function() {
+  return this.last - this.first;
+};
 
--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+
+  instance.storage = {};
+  instance.first = 0;
+  instance.last = 0;
+
+  return instance;
+
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(string) {
+  this.storage[this.last++] = string;
+};
+
+queueMethods.dequeue = function() {
+  if (this.first < this.last) {
+    var frontItem = this.storage[this.first];
+    delete this.storage[this.first++];
+    return frontItem;
+  }
+};
+
+queueMethods.size = function() {
+  return this.last - this.first;
+};
 
