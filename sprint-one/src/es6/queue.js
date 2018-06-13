c6f7837da9adbcab441ca07f4d00108b3ee61b9d
parkermuir <parker.muir@gmail.com>
2018-06-13 19:25:22 -0400

    Completed ES6 implementations
    - ES6 stack instantiation
    - ES6 queue instantiation

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,25 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(storage, first, last) {
+    this.storage = {};
+    this.first = 0;
+    this.last = 0;
   }
 
+  enqueue(string) {
+    this.storage[this.last++] = string;
+  }
+
+  dequeue() {
+    if (this.first < this.last) {
+      var firstItem = this.storage[this.first];
+      delete this.storage[this.first++];
+      return firstItem;
+    }
+  }
+
+  size() {
+    return this.last - this.first;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,24 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(storage, counter) {
+    this.storage = {};
+    this.counter = 0;
   }
 
+  push(string) {
+    this.storage[this.counter++] = string;
+  }
+
+  pop() {
+    if (this.counter > 0) {
+      var popped = this.storage[--this.counter];
+      delete this.storage[this.counter];
+      return popped;
+    }
+  }
+
+  size() {
+    return this.counter;
+  }
 }
