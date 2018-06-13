cd131302c566d6dc15611b96e0fc73bd0f427003
parkermuir <parker.muir@gmail.com>
2018-06-13 18:15:18 -0400

    Working through stack with each instantiation type
    - Completed functional-shared instantiation
    - Completed prototypal instantiation

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  instance.counter = 0;
+
+  _.extend(instance, stackMethods);
+
+  return instance;
 };
 
-var stackMethods = {};
+stackMethods = {
+  
+  push: function(string) {
+    this.storage[this.counter++] = string;
+  },
 
+  pop: function() {
+    if (this.counter > 0) {
+      var popped = this.storage[--this.counter];
+      delete this.storage[this.counter];
+      return popped;
+    }
+  },
+
+  size: function() {
+    return this.counter;
+  }
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+
+  instance.storage = {};
+  instance.counter = 0;
+
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(string) {
+  this.storage[this.counter++] = string;
+};
 
+stackMethods.pop = function() {
+  if (this.counter > 0) {
+    var popped = this.storage[--this.counter];
+    delete this.storage[this.counter];
+    return popped;
+  }
+  
+};
+
+stackMethods.size = function() {
+  return this.counter;
+};
