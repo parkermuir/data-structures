fd33d1b309bc1629de02221ccd42cc9c93a25681
parkermuir <parker.muir@gmail.com>
2018-06-13 22:36:44 -0400

    Completed LinkedList function
    - addToTail, removeHead, contains

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+
+    if (this.tail === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.head.next = node;
+      this.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var currHead = this.head;
+    list.head = list.head.next;
+    return currHead.value;
   };
 
   list.contains = function(target) {
+    //maybe better to try using recursion here?
+    var current = list.head;
+    
+    while (current !== null) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
