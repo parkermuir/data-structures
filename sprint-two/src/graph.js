317cd4aa7e4b78d3a2e399e5c54dee667d55dc58
parkermuir <parker.muir@gmail.com>
2018-06-14 15:39:55 -0400

    Completed the tree data structure section
    -All tests passing for functions
    -Attempted to assign timecomplexities to each

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,38 +2,80 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
+  this.edges = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  //needs an identifier
+  //needs a collection of it's edges
+  this.nodes[node] = null;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.nodes.hasOwnProperty(node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  //remove a node
+  delete this.nodes[node];
+
+  //and remove all of it's edges from the edge list
+  for (edge in this.edges) {
+    if (this.edges[edge].endpoint1 === node || this.edges[edge].endpoint2 === node) {
+      delete this.edges[edge];
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  //check for a connection between two different nodes - referenced by their IDs
+  var hasEdge1 = this.edges.hasOwnProperty(fromNode + 'to' + toNode);
+  var hasEdge2 = this.edges.hasOwnProperty(toNode + 'to' + fromNode);
+  return hasEdge1 || hasEdge2;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  //adds an edge into each node specified by ID
+  this.edges[fromNode + 'to' + toNode] = {'endpoint1': fromNode, 'endpoint2': toNode};
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.edges[fromNode + 'to' + toNode];
+  delete this.edges[toNode + 'to' + fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (node in this.nodes) {
+    cb(node);
+  }
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+Complexity: What is the time complexity of the above functions?
+
+addNode -- O(1)
+contains -- O(1)
+removeNode -- O(n)
+hasEdge -- O(1)
+addEdge -- O(1)
+removeEdge -- O(1)
+forEachNode -- O(n)
+
+
+ 
+ 
+ 
+ 
+ 
+ 
  */
 
 
