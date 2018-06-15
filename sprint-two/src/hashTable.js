b5cc21bc957751bac9b3c6aba3874dd9c07f03ae
parkermuir <parker.muir@gmail.com>
2018-06-14 20:12:30 -0400

    Completed hash-table
    -All implementations are passing tests
    -Refactored to use object storage at each index to help with overflow

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,4 +1,5 @@
 
+//has function provided is getIndexBelowMaxForKey
 
 var HashTable = function() {
   this._limit = 8;
@@ -7,14 +8,27 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var temp = this._storage.get(index);
+  if (temp === undefined) {
+    var temp = {};
+  }
+  temp[k] = v;
+  this._storage.set(index, temp);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var values = this._storage.get(index);
+
+  return values[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var temp = this._storage.get(index);
+  delete temp[k];
+  this._storage.set(index, temp);
 };
 
 
