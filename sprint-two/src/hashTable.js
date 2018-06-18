22fa4c9f1d8e800bdf070d3f1318a133d91c37ca
parkermuir <parker.muir@gmail.com>
2018-06-18 08:07:02 -0400

    Refactored hashtable funcs post-solution lecture
    - Cleaned up readability and flow with tuple and bucket variable names rather than bucket[i][1]
    - Refactored hashTable.remove to use for loop rather than forEach for better time complexity

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,35 +9,31 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  if (this._storage.get(index) === undefined) {
-    var bucket = [[k, v]];
-    this._storage.set(index, bucket);
-  } else {
-    var bucket = this._storage.get(index);
+  var bucket = this._storage.get(index) || [];
 
-    var bucketIndex = -1;
-    for (var i = 0; i < bucket.length; i++) {
-      if (bucket[i][0] === k) {
-        bucketIndex = i;
-      } 
-    }
-
-    if (bucketIndex > -1) {
-      bucket[bucketIndex][1] = v;
-    } else {
-      bucket[bucket.length] = [k, v];
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      var temp = tuple[1];
+      tuple[1] = v;
+      this._storage.set(index, bucket);
+      return temp;
     }
   }
+
+  bucket.push([k, v]);
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
   var bucket = this._storage.get(index);
-  console.log('Inside retrieve : ', bucket);
+
   for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
-      return bucket[i][1];
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
     }
   }
 };
@@ -45,12 +41,39 @@ HashTable.prototype.retrieve = function(k) {
 HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  var bucket = this._storage.get(index);
-  bucket.forEach(function (item, i) {
-    if (item[0] === k) {
+  var bucket = this._storage.get(index, k);
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      var temp = tuple[1];
       bucket.splice(i, 1);
+      return temp;
     }
-  });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // var bucket = this._storage.get(index);
+  // bucket.forEach(function (item, i) {
+  //   if (item[0] === k) {
+  //     bucket.splice(i, 1);
+  //   }
+  // });
 };
 
 /*
